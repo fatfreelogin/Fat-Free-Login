2e2633311238194a891d2a406343f864f8e97abd
peterreyskens <peter@reyskens.net>
2019-05-23 13:39:05 +0200

    Update create_db.sql

--- db/create_db.sql
+++ db/create_db.sql
@@ -21,4 +21,4 @@ ALTER TABLE `users`
 COMMIT;
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `hash`, `activated`, `user_type`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@mail.be', '$2y$10$65g7M7Zpx5v7Mk65T59Vf.zREqI3m2gkpa/vaOHdpSGhf0E92On6q', '', 1, 100, NULL, '2019-03-27 13:57:32');
+(1, 'admin', 'admin@mail.invalid', '$2y$10$65g7M7Zpx5v7Mk65T59Vf.zREqI3m2gkpa/vaOHdpSGhf0E92On6q', '', 1, 100, NULL, '2019-03-27 13:57:32');
