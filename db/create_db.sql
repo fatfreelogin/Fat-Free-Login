3e8fdc41028cf2aa6facca3f40a1bfa7db031fa2
peterreyskens <peter@reyskens.net>
2020-01-26 22:21:52 +0100

    htaccess protect all folders + added logs folder

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE `users` (
+CREATE TABLE `stock_users` (
   `id` int(11) NOT NULL,
   `username` varchar(40) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
@@ -13,12 +13,12 @@ CREATE TABLE `users` (
 
 
 
-ALTER TABLE `users`
+ALTER TABLE `stock_users`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `users`
+ALTER TABLE `stock_users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `hash`, `activated`, `user_type`, `created_at`, `updated_at`) VALUES
+INSERT INTO `stock_users` (`id`, `username`, `email`, `password`, `hash`, `activated`, `user_type`, `created_at`, `updated_at`) VALUES
 (1, 'admin', 'admin@mail.invalid', '$2y$10$65g7M7Zpx5v7Mk65T59Vf.zREqI3m2gkpa/vaOHdpSGhf0E92On6q', '', 1, 100, NULL, '2019-03-27 13:57:32');
