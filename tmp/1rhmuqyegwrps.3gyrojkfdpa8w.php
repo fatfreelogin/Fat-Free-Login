87a735fc71ad4d247c637c8dc2b77fdbc2e54b00
arblickspule <42821506+arblickspule@users.noreply.github.com>
2023-02-14 18:08:43 +0000

    Shift over to Bulma

--- tmp/1rhmuqyegwrps.3gyrojkfdpa8w.php
+++ tmp/1rhmuqyegwrps.3gyrojkfdpa8w.php
@@ -3,60 +3,113 @@
 	<head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<base href="<?= ($SCHEME.'://'.$HOST.$BASE.'/') ?>">
+	<base href=" <?= ($SCHEME.'://'.$HOST.$BASE.'/') ?> ">
 	<title>Brighter Taxis Ltd</title>
-	<link href="https://cdn.jsdelivr.net/npm/daisyui@2.50.0/dist/full.css" rel="stylesheet" type="text/css" />
-	<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" />
   </head>
 
   <body>
-    <div class="flex flex-col h-screen justify-between">
-      <header class="h-14">
-        <div class="navbar bg-base-100">
-          <div class="flex-1">
-            <a class="btn btn-ghost normal-case text-xl">Brighter Taxis Ltd</a>
+      <header>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <a class="navbar-item">
+              Brighter Taxis Ltd
+            </a>
+        
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
-          <div class="flex-none">
-            <?php if ($logged_in==TRUE): ?>
-              <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                  <div class="w-10 rounded-full">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path fill="none" d="M0 0h24v24H0z"></path> <path fill-rule="nonzero" d="M12 2a8.5 8.5 0 0 1 8.5 8.5c0 6.5-5.5 12-8.5 12s-8.5-5.5-8.5-12A8.5 8.5 0 0 1 12 2zm0 2a6.5 6.5 0 0 0-6.5 6.5c0 4.794 4.165 10 6.5 10s6.5-5.206 6.5-10A6.5 6.5 0 0 0 12 4zm5.5 7c.16 0 .319.008.475.025a4.5 4.5 0 0 1-4.95 4.95A4.5 4.5 0 0 1 17.5 11zm-11 0a4.5 4.5 0 0 1 4.475 4.975 4.5 4.5 0 0 1-4.95-4.95C6.18 11.008 6.34 11 6.5 11z"></path> </g> </g></svg>
-                  </div>
-                </label>
-                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                  <li>
-                    <a class="justify-between">
-                      Profile
-                      <span class="badge">New</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a class="justify-between" href="/logout">
-                    Logout
-                    <span class="badge">
-                      <svg class="h-5 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path fill="none" d="M0 0h24v24H0z"></path> <path d="M5 11h8v2H5v3l-5-4 5-4v3zm-1 7h2.708a8 8 0 1 0 0-12H4A9.985 9.985 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10a9.985 9.985 0 0 1-8-4z"></path> </g> </g></svg></li>
-                    </span>
-                  </a>
-                </ul>
-              </div>
-            <?php endif; ?>
-            <?php if ($logged_in==FALSE): ?>
-                <a href="/login">
-                  <button class="btn btn-circle">
-                    <svg class="h-10 w-10" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path fill="none" d="M0 0h24v24H0z"></path> <path d="M10 11V8l5 4-5 4v-3H1v-2h9zm-7.542 4h2.124A8.003 8.003 0 0 0 20 12 8 8 0 0 0 4.582 9H2.458C3.732 4.943 7.522 2 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10c-4.478 0-8.268-2.943-9.542-7z"></path> </g> </g></svg>
-                  </button>
+        
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item">
+                Home
+              </a>
+        
+              <a class="navbar-item">
+                Documentation
+              </a>
+        
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                  More
                 </a>
+        
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">
+                    About
+                  </a>
+                  <a class="navbar-item">
+                    Jobs
+                  </a>
+                  <a class="navbar-item">
+                    Contact
+                  </a>
+                  <hr class="navbar-divider">
+                  <a class="navbar-item">
+                    Report an issue
+                  </a>
+                </div>
               </div>
-            <?php endif; ?> 
+            </div>
+            <?php if (isset($SESSION['logged_in']) && $SESSION['logged_in']): ?>
+              
+                <div class="navbar-item has-dropdown is-hoverable">
+                  <a class="navbar-link">
+                    More
+                  </a>
+          
+                  <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                      Notifications
+                    </a>
+                    <a class="navbar-item">
+                      Account
+                    </a>
+                    <a class="navbar-item">
+                      Contact
+                    </a>
+                    <hr class="navbar-divider">
+                    <a class="navbar-item" href="/logout">
+                      Logout
+                    </a>
+                  </div>
+                </div>
+              
+              <?php else: ?>
+                <!-- <div class="navbar-end">
+                  <div class="navbar-item">
+                    <div class="buttons">
+                      <a class="button is-primary">
+                        <strong>Sign up</strong>
+                      </a>
+                      <a class="button is-light">
+                        Log in
+                      </a>
+                    </div>
+                  </div>
+                </div> -->
+                <div class="navbar-item has-dropdown is-hoverable">
+                  <a class="navbar-link">
+                    Login
+                  </a>
+                  <div class="navbar-dropdown">
+                    <form action="<?= ($BASE) ?>" method="post" class="">
+                      <input type="text" class="form-control" placeholder="Email" name="username" id="username" required autofocus>
+                      <input type="password" class="form-control" placeholder="Password" name="password" id="password" required>
+                      <input type="hidden" name="login" value="login" />
+                      <input type="hidden" name="session_csrf" value="<?= ($CSRF) ?>" />
+                      <button class="btn" type="submit">Login</button>
+                    </form>
+                  </div>
+                </div>
+              
+            <?php endif; ?>
           </div>
-        </div>
+        </nav>
       </header>
 
-      <main class="mb-auto h-10 flex-grow">
+      
--- tmp/1rhmuqyegwrps.laaa7rm999i6.php
+++ tmp/1rhmuqyegwrps.laaa7rm999i6.php
@@ -1,5 +1,5 @@
 
-	</main>
+
 	<!-- /.container -->
 
 	<!-- Footer -->
