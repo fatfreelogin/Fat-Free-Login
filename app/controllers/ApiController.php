02c2ac2bca281216b2e397bdcecd7cbf9017a5ee
Spurblick Ale <42821506+arblickspule@users.noreply.github.com>
2023-02-26 13:39:06 +0000

    set  up api controller

---
+++ app/controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+class ApiController extends Controller {
+	
+	protected $f3;
+	protected $db;
+
+  function __construct() {
+		$f3=Base::instance();
+		$db=new DB\SQL(
+			$f3->get('db_dns') . $f3->get('db_name'),
+			$f3->get('db_user'),
+			$f3->get('db_pass')
+		);
+		$this->f3=$f3;
+		$this->db=$db;
+    // Do we need this?
+    $this->web = \Web::instance();
+	}
+
+  public function test1 () {
+    echo "API Test 1".PHP_EOL;
+    echo "Doesn't actually do anything yet... :D".PHP_EOL;
+  }
+}
